be3dcb83528765ce52a0f0823a5ae38bca546b40
Daniel <102192140+4teq@users.noreply.github.com>
2024-12-23 12:04:36 +0300

    removing all non enhanced images from index page

--- js/testing.js
+++ js/testing.js
@@ -1,8 +1,7 @@
  import EmblaCarousel from 'embla-carousel';
 
-
 const emblaNode = document.querySelector('.embla');
-const options = { loop: true };
-const emblaApi = EmblaCarousel(emblaNode, options);
+const options = {loop: true};
 
-console.log(emblaApi.slideNodes());  // Access API
+const emblaApi = EmblaCarousel(emblaNode, options);
+console.log(emblaApi.slideNodes());
